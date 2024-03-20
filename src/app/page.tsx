1e4e7656415110a6058911e5a6142ae9a3307a4f
Juan Segundo <juansepincha@gmail.com>
2024-03-20 14:05:53 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <Plans />
       <section className="flex flex-col items-center justify-start my-14 px-4 md:px-10 lg:px-20 max-w-[1350px] mx-auto">
         <div className="flex flex-col justify-center items-center gap-4">
-          <Player src={"/videos/video-aplicacion.webm"} />
+          <Player src={"/videos/video-aplicacion-1.webm"} />
           <ScrollLink href="#planes">
             <Button
               background="bg-white"
