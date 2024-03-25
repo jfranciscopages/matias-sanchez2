411cbab733f323873e0f6e3b274fb581caa1b298
Francisco <zextfpages@correoargentino.com.ar>
2024-03-24 21:07:52 -0300

    api temporary fix

--- src/app/api/send-mail-admin/route.ts
+++ src/app/api/send-mail-admin/route.ts
@@ -2,22 +2,22 @@ import { Resend } from "resend";
 import { NextRequest } from "next/server";
 import EmailAdmin from "../../../components/Emails/EmailAdmin";
 
-const resend = new Resend(process.env.RESEND_KEY);
+// const resend = new Resend(process.env.RESEND_KEY);
 
 export async function POST(request: NextRequest) {
   const { name, email, dni } = await request.json();
 
   try {
-    await resend.emails.send({
-      from: "admin@matisanchezsorondo.com",
-      to: ["juansegundomartinez7@gmail.com"],
-      subject: `Mati! ${name} se ha unido al equipo!`,
-      react: EmailAdmin({
-        user: name,
-        dni: dni,
-        email: email,
-      }),
-    });
+    // await resend.emails.send({
+    //   from: "admin@matisanchezsorondo.com",
+    //   to: ["juansegundomartinez7@gmail.com"],
+    //   subject: `Mati! ${name} se ha unido al equipo!`,
+    //   react: EmailAdmin({
+    //     user: name,
+    //     dni: dni,
+    //     email: email,
+    //   }),
+    // });
 
     return Response.json({ message: "Email Sent" }, { status: 200 });
   } catch (error) {
