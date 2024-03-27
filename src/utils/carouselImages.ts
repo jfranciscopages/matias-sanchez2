28ffa13c98fa273f2b011b61ded90c9c57d1be9b
Francisco <zextfpages@correoargentino.com.ar>
2024-03-27 19:51:43 -0300

    transformaciones update

--- src/utils/carouselImages.ts
+++ src/utils/carouselImages.ts
@@ -2,6 +2,7 @@ export const carouselImages = [
   "/imgs/foto1.webp",
   "/imgs/foto2.webp",
   "/imgs/foto3.webp",
+  "/imgs/foto3.5.webp",
   "/imgs/foto4.webp",
   "/imgs/foto5.webp",
   "/imgs/foto6.webp",
