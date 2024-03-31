92d11e0531f46abc4987c4109356b963edfcd5d7
Francisco <zextfpages@correoargentino.com.ar>
2024-03-31 18:56:38 -0300

    timer v3

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -9,15 +9,14 @@ const Timer = () => {
     const timer = setInterval(() => {
       setSecondsRemaining((prevSeconds) => {
         if (prevSeconds === 0) {
-          clearInterval(timer);
-          // ends
+          setSecondsRemaining(initialTime);
         }
         return prevSeconds - 1;
       });
     }, 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [initialTime]);
 
   const formatTime = (timeInSeconds) => {
     const minutes = Math.floor(timeInSeconds / 60);
@@ -27,20 +26,48 @@ const Timer = () => {
 
   const { minutes, seconds } = formatTime(secondsRemaining);
 
+  const [vacancies, setVacancies] = useState(20);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const decreaseAmount = Math.floor(Math.random() * 2) + 1;
+      setVacancies(prevVacancies => {
+        const newVacancies = prevVacancies - decreaseAmount;
+        return newVacancies >= 0 ? newVacancies : 0;
+      });
+    }, 62000); 
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
-      <div className="grid grid-flow-col gap-3 text-center justify-center auto-cols-max">
-        <div className="flex flex-col text-base">
-          <span className="countdown lg:text-4xl md:text-4xl sm:text-sm">
-          {minutes.toString().padStart(2, "0")}
-          </span>
-          min
+      <div
+        id="countdown-timer"
+        className="fixed grid grid-flow-col gap-1 text-center justify-center auto-cols-max md:right-6 z-[100]"
+      >
+        <div className="flex flex-row justify-center pt-2 md:pt-0 md:gap-x-1">
+          <div className="flex flex-col text-sm md:text-base md:leading-4 md:min-w-16 items-center min-w-10 leading-none">
+            <span className="countdown lg:text-5xl md:text-4xl text-2xl">
+              {minutes.toString().padStart(2, "0")}
+            </span>
+            min
+          </div>
+          <div className="flex flex-col text-sm md:text-base md:leading-4 md:min-w-16 items-center min-w-10 leading-none">
+            <span className="countdown lg:text-5xl md:text-4xl text-2xl">
+              {seconds.toString().padStart(2, "0")}
+            </span>
+            seg
+          </div>
         </div>
-        <div className="flex flex-col text-base">
-          <span className="countdown lg:text-4xl md:text-4xl sm:text-sm">
-          {seconds.toString().padStart(2, "0")}
-          </span>
-          seg
+        <div className="flex flex-col text-base pt-1 md:pt-px">
+          <p className="text-sm md:text-xl hidden md:flex">¡ÚLTIMOS CUPOS!</p>
+          <p className="text-sm md:text-xl whitespace-nowrap hidden md:flex">
+            <span className="font-extrabold text-blue px-0.5">{vacancies}</span> DISPONIBLES
+          </p>
+          <p className="text-sm md:hidden whitespace-nowrap">
+            ¡ÚLTIMOS <span className="font-extrabold text-blue px-0.5">{vacancies}</span> CUPOS DISPONIBLES!
+          </p>
         </div>
       </div>
     </>
