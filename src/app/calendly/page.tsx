e9204702d42b97838f5dc386ba27a14abf033029
valentin-hechter <valentin.hechter@latechfactory.com>
2024-05-30 17:45:45 -0300

    font size

--- src/app/calendly/page.tsx
+++ src/app/calendly/page.tsx
@@ -7,11 +7,11 @@ const Calendly = () => {
     <section className="w-11/12 sm:w-11/12  md:w-7/12 flex flex-col items-center justify-center gap-5 md:gap-0 md:justify-between max-w-[1350px] mx-auto bg-gradient-to-b from-black to-gray-800">
       <div className=" p-6 w-full bg-white rounded-2xl flex flex-col items-center justify-center h-[1350px]">
         <div className="flex flex-col justify-center items-center ">
-          <h2 className="text-black font-bold text-3xl text-center">
+          <h2 className="text-black font-bold text-2xl text-center">
             ¿Cómo Empezar?
           </h2>
           <br />
-          <h2 className="text-black font-bold text-xl text-center">
+          <h2 className="text-black font-bold text-l text-center">
             <span className="text-blue">Agenda una llamada</span> en el
             calendario de abajo para ver <br /> si sos un buen fit para nuestro
             programa!
