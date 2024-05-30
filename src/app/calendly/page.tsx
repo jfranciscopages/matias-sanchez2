c5c678199a4c79f6c3eb33be442809fbfd935d25
valentin-hechter <valentin.hechter@latechfactory.com>
2024-05-30 16:48:17 -0300

    fix calendly

--- src/app/calendly/page.tsx
+++ src/app/calendly/page.tsx
@@ -4,7 +4,7 @@ import CalendlyWidget from "../../components/calendly";
 
 const Calendly = () => {
   return (
-    <section className="sm:w-11/12  md:w-7/12 flex flex-col items-center justify-center gap-5 md:gap-0 md:justify-between max-w-[1350px] mx-auto bg-gradient-to-b from-black to-gray-800">
+    <section className="w-11/12 sm:w-11/12  md:w-7/12 flex flex-col items-center justify-center gap-5 md:gap-0 md:justify-between max-w-[1350px] mx-auto bg-gradient-to-b from-black to-gray-800">
       <div className=" p-6 w-full bg-white rounded-2xl flex flex-col items-center justify-center h-[1350px]">
         <div className="flex flex-col justify-center items-center ">
           <h2 className="text-black font-bold text-3xl text-center">
--- src/components/calendly.tsx
+++ src/components/calendly.tsx
@@ -1,10 +1,10 @@
 // components/CalendlyWidget.tsx
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const CalendlyWidget: React.FC = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
     script.async = true;
     document.body.appendChild(script);
 
@@ -15,7 +15,7 @@ const CalendlyWidget: React.FC = () => {
 
   return (
     <div
-      className="calendly-inline-widget min-w-[320px] w-[700px] h-[1150px]"
+      className="calendly-inline-widget min-w-[320px] w-full h-[1150px]"
       data-url="https://calendly.com/matiassanchezsorondo/asesoria"
     ></div>
   );
